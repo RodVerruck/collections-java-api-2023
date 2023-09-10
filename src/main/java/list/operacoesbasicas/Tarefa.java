e329cdae2f0eb4700a911dd4a2e57c8419939903
Rodrigo Verruck Pedro <rodrigoverruck@gmail.com>
2023-09-10 17:48:32 -0300

    Criado novos arquivos

---
+++ src/main/java/list/operacoesbasicas/Tarefa.java
@@ -0,0 +1,10 @@
+package main.java.list.operacoesbasicas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+}
