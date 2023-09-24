7305718641f5f279c5b3da267bcb8b09ef372e90
Rodrigo Verruck Pedro <rodrigoverruck@gmail.com>
2023-09-24 09:32:34 -0300

    teste
    Signed-off-by: Rodrigo Verruck Pedro <rodrigoverruck@gmail.com>

---
+++ src/main/java/map/operacoesbasicas/AgendaContatos.java
@@ -0,0 +1,15 @@
+package main.java.map.operacoesbasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone) {
+    }
+}
